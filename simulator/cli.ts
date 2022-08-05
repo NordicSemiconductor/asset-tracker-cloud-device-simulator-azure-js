e1c57d5e17ed0f22baf548fbcf24b3360091bbe4
Markus Tacker <Markus.Tacker@NordicSemi.no>
2022-08-05 12:10:10 +0200

    fix: typo

--- simulator/cli.ts
+++ simulator/cli.ts
@@ -4,7 +4,7 @@ import { simulator } from '#simulator/simulator.js'
 import chalk from 'chalk'
 
 const die = (err: Error) => {
-	console.error(chalk.red(`An unhandled exception occured!`))
+	console.error(chalk.red(`An unhandled exception occurred!`))
 	console.error(chalk.red(err.message))
 	console.error(err)
 	process.exit(1)
