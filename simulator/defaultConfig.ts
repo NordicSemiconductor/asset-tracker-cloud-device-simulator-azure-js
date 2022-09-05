9937f924db0e8628d397fe02f066fb389f093bf2
Mauro López Jiménez <mauro.lopez@nordicsemi.no>
2022-09-05 15:19:18 +0200

    feat(protocol): add support for new accelerometer config See NordicSemiconductor/asset-tracker-cloud-docs#520
    BREAKING CHANGE: this changes the cloud protocol

--- simulator/defaultConfig.ts
+++ simulator/defaultConfig.ts
@@ -4,5 +4,7 @@ export const defaultConfig = {
 	mvres: 300, // (movement resolution) In passive mode: Time in seconds to wait after detecting movement before sending the next update
 	mvt: 3600, // (movement timeout) In passive mode: Send update at least this often (in seconds)
 	gpst: 60, // GPS timeout (in seconds): timeout for GPS fix
-	acct: 0.1, // Accelerometer threshold: minimal absolute value for and accelerometer reading to be considered movement.
+	accath: 10.5, // Accelerometer activity threshold in m/s²: Minimal absolute value for an accelerometer reading to be considered movement.
+	accith: 5.2, // Accelerometer inactivity threshold in m/s²: Maximum absolute value for an accelerometer reading to be considered stillness. Should be lower than the activity threshold.
+	accito: 1.7, // Accelerometer inactivity timeout in s: Hysteresis timeout for stillness detection.
 } as const
